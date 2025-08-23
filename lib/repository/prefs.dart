f9a50cc56801e896844424808381b1589e6979b6
render1980 <render1980@gmail.com>
2025-08-23 19:30:27 +0300

    Use prefix: will be reused for shared projects as well

--- lib/repository/prefs.dart
+++ lib/repository/prefs.dart
@@ -1,109 +1,201 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class SharedPreferencesRepository {
   final prefsFuture = SharedPreferences.getInstance();
 
   // Categories
-  Future<List<String>> loadCategories() async {
+  Future<List<String>> loadCategories(String prefix) async {
     final prefs = await prefsFuture;
-    return prefs.getStringList('needsly.categories') ?? [];
+    return prefs.getStringList(prefix) ?? [];
   }
 
-  Future<void> saveCategories(List<String> categories) async {
+  Future<void> saveCategories(String prefix, List<String> categories) async {
     final prefs = await prefsFuture;
-    await prefs.setStringList('needsly.categories', categories);
+    await prefs.setStringList(prefix, categories);
   }
 
-  Future<void> removeCategory(int idx) async {
+  Future<void> removeCategory(String prefix, int idx) async {
     final prefs = await prefsFuture;
-    final categories = prefs.getStringList('needsly.categories') ?? [];
+    final categories = prefs.getStringList(prefix) ?? [];
     final removedCategory = categories.removeAt(idx);
-    saveCategories(categories);
-    removeSubcategories(removedCategory);
+    saveCategories(prefix, categories);
+    _removeSubcategories(prefix, removedCategory);
   }
 
-  Future<void> renameCategory(int idx, String fromCategory, String toCategory) async {
+  Future<void> renameCategory(
+    String prefix,
+    int idx,
+    String fromCategory,
+    String toCategory,
+  ) async {
     final prefs = await prefsFuture;
-    final categories = prefs.getStringList('needsly.categories') ?? [];
+    final categories = prefs.getStringList(prefix) ?? [];
     categories[idx] = toCategory;
 
-    final subcategories = await loadSubcategories(fromCategory);
+    final subcategories = await loadSubcategories(prefix, fromCategory);
 
     for (var subcategory in subcategories) {
-      final items = await loadItems(fromCategory, subcategory);
-      addSubcategoryWithItems(toCategory, subcategory, items);
+      final items = await loadItems(prefix, fromCategory, subcategory);
+      addSubcategoryWithItems(prefix, toCategory, subcategory, items);
     }
 
-    removeSubcategories(fromCategory);
-    saveCategories(categories);
+    _removeSubcategories(prefix, fromCategory);
+    saveCategories(prefix, categories);
   }
 
   // Subcategories
-  Future<List<String>> loadSubcategories(String category) async {
+  Future<List<String>> loadSubcategories(String prefix, String category) async {
     final prefs = await prefsFuture;
-    return prefs.getStringList('needsly.$category') ?? [];
+    return prefs.getStringList('$prefix.$category') ?? [];
   }
 
-  void updateSubcategories(String category, List<String> subcategories) async {
+  void updateSubcategories(
+    String prefix,
+    String category,
+    List<String> subcategories,
+  ) async {
     final prefs = await prefsFuture;
-    await prefs.setStringList('needsly.$category', subcategories);
+    await prefs.setStringList('$prefix.$category', subcategories);
   }
 
-  void removeSubcategory(String category, String subcategory) async {
+  void _removeSubcategory(
+    String prefix,
+    String category,
+    String subcategory,
+  ) async {
     final prefs = await prefsFuture;
-    await prefs.remove('needsly.$category.$subcategory');
-    final subcategories = prefs.getStringList('needsly.$category') ?? [];
+    await prefs.remove('$prefix.$category.$subcategory');
+    final subcategories = prefs.getStringList('$prefix.$category') ?? [];
     subcategories.remove(subcategory);
-    updateSubcategories(category, subcategories);
+    updateSubcategories(prefix, category, subcategories);
   }
-  
-  void removeSubcategories(String category) async {
+
+  void _removeSubcategories(String prefix, String category) async {
     final prefs = await prefsFuture;
-    final subcategoriesToRemove = prefs.getStringList('needsly.$category') ?? [];    
-    await prefs.remove('needsly.$category');
-    removeItems(category, subcategoriesToRemove);
+    final subcategoriesToRemove =
+        prefs.getStringList('$prefix.$category') ?? [];
+    await prefs.remove('$prefix.$category');
+    _removeItems(prefix, category, subcategoriesToRemove);
   }
 
   void renameSubcategory(
+    String prefix,
     String category,
     String fromSubcategory,
     String toSubcategory,
     List<String> items,
   ) async {
-    addSubcategoryWithItems(category, toSubcategory, items);
-    removeSubcategory(category, fromSubcategory);
+    addSubcategoryWithItems(prefix, category, toSubcategory, items);
+    _removeSubcategory(prefix, category, fromSubcategory);
   }
 
   // Items
   void addSubcategoryWithItems(
+    String prefix,
     String category,
     String subcategory,
     List<String> items,
   ) async {
     final prefs = await prefsFuture;
-    final subcategories = prefs.getStringList('needsly.$category') ?? [];
-    await prefs.setStringList('needsly.$category.$subcategory', items);
-    updateSubcategories(category, [...subcategories, subcategory]);
+    final subcategories = prefs.getStringList('$prefix.$category') ?? [];
+    await prefs.setStringList('$prefix.$category.$subcategory', items);
+    updateSubcategories(prefix, category, [...subcategories, subcategory]);
   }
 
-  Future<List<String>> loadItems(String category, String subcategory) async {
+  Future<List<String>> loadItems(
+    String prefix,
+    String category,
+    String subcategory,
+  ) async {
     final prefs = await prefsFuture;
-    return prefs.getStringList('needsly.$category.$subcategory') ?? [];
+    return prefs.getStringList('$prefix.$category.$subcategory') ?? [];
   }
 
   Future<void> saveItems(
+    String prefix,
     String category,
     String subcategory,
     List<String> items,
   ) async {
     final prefs = await prefsFuture;
-    await prefs.setStringList('needsly.$category.$subcategory', items);
+    await prefs.setStringList('$prefix.$category.$subcategory', items);
   }
 
-  Future<void> removeItems(String category, List<String> subcategories) async {
+  Future<void> _removeItems(
+    String prefix,
+    String category,
+    List<String> subcategories,
+  ) async {
     final prefs = await prefsFuture;
     for (var subcategory in subcategories) {
-      await prefs.remove('needsly.$category.$subcategory');
+      await prefs.remove('$prefix.$category.$subcategory');
     }
   }
+
+  // Shared projects
+  Future<void> updateFirebaseProjectCreds(
+    String projectName,
+    String apiKey,
+    String appId,
+    String projectId,
+    String messagingSenderId,
+  ) async {
+    final prefs = await prefsFuture;
+    prefs.setString(
+      'needsly.firebase.$projectName.credentials.api_key',
+      apiKey,
+    );
+    prefs.setString('needsly.firebase.$projectName.credentials.app_id', appId);
+    prefs.setString(
+      'needsly.firebase.$projectName.credentials.project_id',
+      projectId,
+    );
+    prefs.setString(
+      'needsly.firebase.$projectName.credentials.messaging_sender_id',
+      messagingSenderId,
+    );
+  }
+
+  Future<FirebaseProjectCreds> loadFirebaseProjectCreds(
+    String projectName,
+  ) async {
+    final prefs = await prefsFuture;
+
+    final apiKey = prefs.getString(
+      'needsly.firebase.$projectName.credentials.api_key',
+    );
+    final appId = prefs.getString(
+      'needsly.firebase.$projectName.credentials.app_id',
+    );
+    final projectId = prefs.getString(
+      'needsly.firebase.$projectName.credentials.project_id',
+    );
+    final senderId = prefs.getString(
+      'needsly.firebase.$projectName.credentials.messaging_sender_id',
+    );
+    return FirebaseProjectCreds(
+      projectName: projectName,
+      apiKey: apiKey,
+      appId: appId,
+      projectId: projectId,
+      messagingSenderId: senderId,
+    );
+  }
+}
+
+class FirebaseProjectCreds {
+  final String projectName;
+  final String? apiKey;
+  final String? appId;
+  final String? projectId;
+  final String? messagingSenderId;
+
+  const FirebaseProjectCreds({
+    required this.projectName,
+    required this.apiKey,
+    required this.appId,
+    required this.projectId,
+    required this.messagingSenderId,
+  });
 }
