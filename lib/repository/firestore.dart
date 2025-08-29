2ba38f54234d79747c8894eba6b60630693c0bfa
render1980 <render1980@gmail.com>
2025-08-29 12:47:13 +0300

    Test firestore operations

--- lib/repository/firestore.dart
+++ lib/repository/firestore.dart
@@ -5,29 +5,50 @@ class FirestoreRepository {
 
   FirestoreRepository({required this.firestore});
 
-  void addDocument(String collection, String document) {
+  void listDocuments(String collection) async {
+    final res = firestore.collection(collection).get().then((snapshot) {
+      for (var doc in snapshot.docs) {
+        print("${doc.id} => ${doc.data()}");
+      }
+    });
+    return await res;
+  }
+
+  Future<dynamic> listItems(String collection, String document) async {
+    final res = firestore.collection(collection).doc(document).get().then((
+      snapshot,
+    ) {
+      final items = snapshot.data()?.values ?? [];
+      for (var i in items) {
+        print("$i");
+      }
+    });
+    return res;
+  }
+
+  Future<void> addDocument(String collection, String document) async {
     firestore.collection(collection).doc(document).set({
       "items": [],
     }, SetOptions(merge: true));
   }
 
-  void deleteDocument(String collection, String document) async {
-    await firestore.collection(collection).doc(document).delete();
+  Future<void> deleteDocument(String collection, String document) async {
+    firestore.collection(collection).doc(document).delete();
   }
 
-  void overwriteDocument(
+  Future<void> overwriteDocument(
     String collection,
     String document,
     Map<String, dynamic> items,
   ) async {
-    await firestore.collection(collection).doc(document).set(items);
+    firestore.collection(collection).doc(document).set(items);
   }
 
-  void updateDocumentWithMerge(
+  Future<void> updateDocumentWithMerge(
     String collection,
     String document,
     Map<String, dynamic> items,
   ) async {
-    await firestore.collection(collection).doc(document).set(items);
+    firestore.collection(collection).doc(document).set(items);
   }
 }
